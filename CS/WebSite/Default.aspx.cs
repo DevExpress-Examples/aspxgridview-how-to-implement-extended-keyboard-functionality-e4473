492c25c095907d32ad5d9f046799e53b20a7eac8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+public partial class _Default : System.Web.UI.Page {
+
+    //remove all event handlers for using
+    protected void Page_Init(object sender, EventArgs e) {
+        gridView.RowDeleting += new DevExpress.Web.Data.ASPxDataDeletingEventHandler(gridView_RowDeleting);
+        gridView.RowInserting += new DevExpress.Web.Data.ASPxDataInsertingEventHandler(gridView_RowInserting);
+        gridView.RowUpdating += new DevExpress.Web.Data.ASPxDataUpdatingEventHandler(gridView_RowUpdating);
+    }
+
+    void gridView_RowUpdating(object sender, DevExpress.Web.Data.ASPxDataUpdatingEventArgs e) {
+        e.Cancel = true;
+        gridView.CancelEdit();
+    }
+
+    void gridView_RowInserting(object sender, DevExpress.Web.Data.ASPxDataInsertingEventArgs e) {
+        e.Cancel = true;
+        gridView.CancelEdit();
+    }
+
+    void gridView_RowDeleting(object sender, DevExpress.Web.Data.ASPxDataDeletingEventArgs e) {
+        throw new Exception("Deleting is not allowed in the online example");
+    }
+
+}
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+
+	'remove all event handlers for using
+	Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+		AddHandler gridView.RowDeleting, AddressOf gridView_RowDeleting
+		AddHandler gridView.RowInserting, AddressOf gridView_RowInserting
+		AddHandler gridView.RowUpdating, AddressOf gridView_RowUpdating
+	End Sub
+
+	Private Sub gridView_RowUpdating(ByVal sender As Object, ByVal e As DevExpress.Web.Data.ASPxDataUpdatingEventArgs)
+		e.Cancel = True
+		gridView.CancelEdit()
+	End Sub
+
+	Private Sub gridView_RowInserting(ByVal sender As Object, ByVal e As DevExpress.Web.Data.ASPxDataInsertingEventArgs)
+		e.Cancel = True
+		gridView.CancelEdit()
+	End Sub
+
+	Private Sub gridView_RowDeleting(ByVal sender As Object, ByVal e As DevExpress.Web.Data.ASPxDataDeletingEventArgs)
+		Throw New Exception("Deleting is not allowed in the online example")
+	End Sub
+
+End Class
